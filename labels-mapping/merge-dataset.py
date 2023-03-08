e3208aee44096147e5da1f6768ed6bb9005086ec
leon <leon.richard.tan@gmail.com>
2023-03-08 23:00:05 +0800

    merge标签的散布工作全部完成

--- labels-mapping/merge-dataset.py
+++ labels-mapping/merge-dataset.py
@@ -19,6 +19,8 @@ IMAGE_FILES_PATH = Path.cwd() / MERGE_CFG_DICT["image-files-path"]
 MANUAL_LABEL_FILES_PATH = Path.cwd() / MERGE_CFG_DICT["manual-label-files-path"]
 YOLOV8X_LABEL_FILES_PATH = LABELS_MAPPING_PATH / MERGE_CFG_DICT["yolov8x-label-files-path"]
 
+MERGED_DATASET_PATH = Path.cwd() / MERGE_CFG_DICT["merged-dataset-path"]
+
 if not IMAGE_FILES_PATH.exists():
     raise NotADirectoryError(f"image files path {IMAGE_FILES_PATH} incorrect!")
 
@@ -30,8 +32,18 @@ if YOLOV8X_LABEL_FILES_PATH.exists():
         shutil.rmtree(YOLOV8X_LABEL_FILES_PATH)
     except OSError as e:
         print("Error: %s : %s" % (YOLOV8X_LABEL_FILES_PATH, e.strerror))
-    # YOLOV8X_LABEL_FILES_PATH.rmdir()
 YOLOV8X_LABEL_FILES_PATH.mkdir(parents=True)
+
+if MERGED_DATASET_PATH.exists():
+    try:
+        shutil.rmtree(MERGED_DATASET_PATH)
+    except OSError as e:
+        print("Error: %s : %s" % (MERGED_DATASET_PATH, e.strerror))
+MERGED_DATASET_PATH.mkdir(parents=True)
+MERGED_LABELS_PATH = MERGED_DATASET_PATH / "labels"
+MERGED_LABELS_PATH.mkdir(parents=True)
+MERGED_IMAGES_PATH = MERGED_DATASET_PATH / "images"
+MERGED_IMAGES_PATH.mkdir(parents=True)
 
 # 解析manual-label-names到merged-label-names的关系，形成mapping表
 manual_label_mapping = {}
@@ -58,25 +70,59 @@ print(yolov8_label_mapping)
 model = YOLO("yolov8x.pt")
 
 # 步骤：
-# 1. 用yolov8x自动为images生成labels，并通过mapping删除78和79号标签；
 for image_file_path in IMAGE_FILES_PATH.rglob("*.jpg"):
     if image_file_path.is_file():
+        ###############################################################
+        # 1. 用yolov8x自动为images生成labels，并通过mapping删除78和79号标签；
+        ###############################################################
         results = model.predict(source=image_file_path, save=True, save_txt=True)
-        
-        save_label_path = str(YOLOV8X_LABEL_FILES_PATH / image_file_path.stem)
-
+    
         class_id, xywhn = results[0].boxes.cls.view(-1,1).cpu().numpy(), results[0].boxes.xywhn.cpu().numpy()
-        numpy_boxes = np.concatenate((class_id, xywhn), axis=1)
+        yolov8_numpy_boxes = np.concatenate((class_id, xywhn), axis=1)
 
         # 把class_id进行mapping
-        for row in numpy_boxes:                         # 遍历二维数组中的每一行
+        for row in yolov8_numpy_boxes:                         # 遍历二维数组中的每一行
             row[0] = yolov8_label_mapping[row[0]]
-        numpy_boxes = np.delete(numpy_boxes, np.where(numpy_boxes[:,0] == -1), axis=0)  # 删除映射成-1的行
+        yolov8_numpy_boxes = np.delete(yolov8_numpy_boxes, np.where(yolov8_numpy_boxes[:,0] == -1), axis=0)  # 删除映射成-1的行
         
-        with open(f'{save_label_path}.txt', 'a') as f:
-            np.savetxt(f, numpy_boxes, fmt='%g')
-            # f.write(str(numpy_boxes))
+        save_label_path = str(YOLOV8X_LABEL_FILES_PATH / image_file_path.stem) + '.txt'
+        with open(save_label_path, mode='w') as f:
+            np.savetxt(f, yolov8_numpy_boxes, fmt='%g')
 
-# 2. 把别人手工标注好的labels，通过mapping映射到78和79号标签；
-# 3. 按照文件名，把上述两类标签合并，并且把image文件也整理到输出目录。
+        ###############################################################
+        # 2. 把别人手工标注好的labels，通过mapping映射到78和79号标签；
+        ###############################################################
+        manual_label_file_path = None
+        for file_path in MANUAL_LABEL_FILES_PATH.rglob("*.txt"):
+            if file_path.is_file() and file_path.name == str(image_file_path.stem) + '.txt':
+                manual_label_file_path = file_path
+        
+        manual_numpy_boxes = np.zeros(shape=(0, 5))
+        if manual_label_file_path is not None:
+            with open(f'{manual_label_file_path}', mode='r') as f:
+                manual_numpy_boxes = np.loadtxt(f, ndmin=2)
 
+        # 把class_id进行mapping
+        for row in manual_numpy_boxes:                         # 遍历二维数组中的每一行
+            row[0] = manual_label_mapping[row[0]]
+        manual_numpy_boxes = np.delete(manual_numpy_boxes, np.where(manual_numpy_boxes[:,0] == -1), axis=0)  # 删除映射成-1的行
+
+        ###############################################################
+        # 3. 按照文件名，把上述两类标签合并，并且把image文件也整理到输出目录。
+        ###############################################################
+        merged_numpy_boxes = np.concatenate((yolov8_numpy_boxes, manual_numpy_boxes), axis=0)
+        merged_label_file_path = str(MERGED_LABELS_PATH / image_file_path.stem) + '.txt'
+        with open(merged_label_file_path, mode='w') as f:
+            np.savetxt(f, merged_numpy_boxes, fmt='%g')
+
+        src_path = image_file_path
+        dst_path = MERGED_IMAGES_PATH / image_file_path.name
+
+        with open(src_path, 'rb') as src_file:
+            with open(dst_path, 'wb') as dst_file:
+                dst_file.write(src_file.read())
+
+# 最后再把class name写入到labels.txt文件中
+with open(MERGED_DATASET_PATH / "labels.txt", 'w') as f:
+    for key, val in MERGE_CFG_DICT['merged-label-names'].items():
+        f.write(val + '\n')
