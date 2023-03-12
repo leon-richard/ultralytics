e0ae0f19a687a2904a45ceaa0e34121349fc71c4
leon <leon.richard.tan@gmail.com>
2023-03-12 22:57:23 +0800

    python程序用cfg文件的配置来训练模型

--- labels-mapping/generate-labels.py
+++ labels-mapping/generate-labels.py
@@ -11,7 +11,7 @@ image_files_list = []
 # from ndarray
 # im2 = cv2.imread("bus.jpg")
 # print(im2)
-results = model.predict(source="../../datasets/from-roboflow/fscbp-proj2/valid/images/191678450999_-pic_jpg.rf.e8555dfbffbbe5fbd0cfbd0566b08fdc.jpg", save=True, save_txt=True)  # save predictions as labels
+results = model.predict(source="../../datasets/from-roboflow/fscbp-proj2/train/images/161678450999_-pic_jpg.rf.f6ca9f4f129050ea891747698b9c33d0.jpg", save=True, save_txt=True)  # save predictions as labels
 # results = model.predict(source="coco128.yaml", save=True, save_txt=True)  # save predictions as labels
 
 print(results)
